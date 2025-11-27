e34e54279d3efac167485728924bc43e4a58e938
Zeeshan <mdz220755@gmail.com>
2025-11-27 23:13:28 +0530

    chore: disable button

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ document
   .addEventListener("submit", async function (event) {
     event.preventDefault();
 
+    const submitBtn = document.querySelector("#leadForm button[type='submit']");
+    const responseMessage = document.getElementById("responseMessage");
+
     const formData = {
       name: document.getElementById("name").value,
       email: document.getElementById("email").value,
       phone: document.getElementById("phone").value,
     };
 
-    const responseMessage = document.getElementById("responseMessage");
-
+    // Phone validation
     if (!/^[0-9]{10}$/.test(formData.phone)) {
       responseMessage.textContent =
         "Please enter a valid 10-digit phone number.";
@@ -18,10 +20,14 @@ document
       return;
     }
 
+    // Disable button
+    submitBtn.disabled = true;
+    submitBtn.textContent = "Submitting...";
     responseMessage.textContent = "Submitting...";
     responseMessage.style.color = "blue";
 
     try {
+
       const response = await fetch(
         "https://script.google.com/macros/s/AKfycbyeCKwLizdE0-NYNewe_TuIZO5PSwcsWlputksMlTtRG0g0NtZCTH6XLU-z2Z3PXcaRYg/exec",
         {
@@ -39,9 +45,14 @@ document
       responseMessage.style.color = "green";
 
       document.getElementById("leadForm").reset();
+      submitBtn.textContent = "Submitted";
     } catch (error) {
       responseMessage.textContent =
         "An error occurred while submitting the form. Please try again.";
       responseMessage.style.color = "red";
+
+      // Re-enable button on error
+      submitBtn.disabled = false;
+      submitBtn.textContent = "Submit";
     }
   });
